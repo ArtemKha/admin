6bf978716323ba2ccb4d69067b50d164d06a33b1
Artem <khaapt@gmail.com>
2017-06-26 18:27:13 +0300

    search redux

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import * as TableActionTypes from '../actions/table';
 import Sidebar from '../components/Sidebar';
 
+const mapStateToProps = state => ({});
+
 const mapDispatchToProps = dispatch => ({
   addRow: bindActionCreators(TableActionTypes.addRow, dispatch),
   selectRow: bindActionCreators(TableActionTypes.selectRow, dispatch),
   showAll: bindActionCreators(TableActionTypes.showAll, dispatch)
 });
 
-export default connect(mapDispatchToProps)(Sidebar);
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
